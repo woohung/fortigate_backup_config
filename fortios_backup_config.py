5b61a986496002e83619a2619da624e12e04f165
woohung <yozeff64@gmail.com>
2023-03-24 02:06:06 +0300

    Разгрузил main, добавил функции: read_devices, process_device, удалил лишние переменные, директория backup_dir теперь создается с правами 700, оформил Docstring.

--- fortios_backup_config.py
+++ fortios_backup_config.py
@@ -7,6 +7,7 @@ import logging
 import argparse
 
 
+
 def fortios_backup_config(
     device,
     token,
@@ -40,9 +41,7 @@ def fortios_backup_config(
     logger = logging.getLogger(__name__)
 
     # Создание токена для авторизации по API
-    unique_api_token = f"Bearer {token}"
-    payload = {}
-    headers = {"Authorization": unique_api_token}
+    headers = {"Authorization": f"Bearer {token}"}
 
     # Формирование URL для выполнения запроса
     url = f"https://{device}/api/v2/monitor/system/config/backup?destination=file&scope=global"
@@ -54,14 +53,14 @@ def fortios_backup_config(
     try:
         # Отправка запроса и обработка исключений
         response = requests.get(
-            url, headers=headers, data=payload, verify=enable_ssl, timeout=10
+            url, headers=headers, verify=enable_ssl, timeout=10
         )
         response.raise_for_status()
     except requests.exceptions.Timeout:
         logger.exception(f"Таймаут подключения, проверь доступность {device}.")
         return
     except requests.exceptions.HTTPError as err:
-        logger.exception(f"HTTP ошибка: {url}, гугли по следующему коду ответа: {err.response.status_code}")
+        logger.exception(f"HTTP ошибка в запросе: {url}, гугли по следующему коду ответа: {err.response.status_code}")
         return
     except requests.exceptions.RequestException as err:
         logger.exception(f"Что-то пошло не так, необходимо дополнительно разобраться, вот код ошибки: {err}")
@@ -71,7 +70,7 @@ def fortios_backup_config(
     full_config = response.text
     current_time = datetime.datetime.today().strftime("%Y_%b_%d")
     backup_path = os.path.join(backup_dir, f"backup_{current_time}")
-    os.makedirs(backup_path, exist_ok=True)
+    os.makedirs(backup_path, exist_ok=True, mode=0o700)
     backup_file_path = os.path.join(backup_path, f"{device}_{current_time}.conf")
 
     # Запись конфигурации в файл
@@ -81,6 +80,12 @@ def fortios_backup_config(
 
 
 def parse_cmd_args():
+    """
+    Функция для парсинга аргументов командной строки.
+
+    Returns:
+        argparse.Namespace: Пространство имен с результатами парсинга аргументов командной строки.
+    """
     # Создаем парсер аргументов командной строки
     parser = argparse.ArgumentParser()
 
@@ -108,25 +113,58 @@ def parse_cmd_args():
     return parser.parse_args()
 
 
-def main():
-    # Разбираем аргументы командной строки, переданные пользователем
-    args = parse_cmd_args()
+def read_devices(config_file):
+    """
+    Функция для чтения файла конфигурации устройств и загрузки его в переменную.
 
-    # Читаем файл конфигурации YAML и загружаем его в переменную `devices`
-    with open(args.config_file) as f:
+    Args:
+        config_file (str): Путь к файлу конфигурации.
+
+    Returns:
+        List[Dict[str, str]]: Список устройств и их параметров в формате словаря.
+    """
+    with open(config_file) as f:
         devices = yaml.safe_load(f)
+    return devices
 
-    # Обрабатываем каждое устройство в списке `devices`
+
+def process_device(device, backup_dir, enable_warning, enable_ssl):
+    """
+    Функция для обработки каждого устройства из списка `devices`.
+
+    Args:
+        device (Dict[str, str]): Словарь с параметрами устройства.
+        backup_dir (str): Директория для сохранения резервных копий.
+        enable_warning (bool): Флаг, включающий/выключающий предупреждения безопасности SSL.
+        enable_ssl (bool): Флаг, включающий/выключающий проверку SSL.
+
+    Returns:
+        None
+    """
+    if "host" not in device or "token" not in device:
+        logging.error(f"Отсутствуют обязательные параметры 'host' или 'token' для устройства {device}")
+        return
+    print(f"{'#'*20} Подключаюсь к {device['host']} {'#'*20}")
+    fortios_backup_config(
+        device["host"],
+        device["token"],
+        backup_dir=backup_dir,
+        enable_warning=enable_warning,
+        enable_ssl=enable_ssl,
+    )
+
+def main():
+    """
+    Главная функция скрипта, выполняющая парсинг аргументов командной строки, чтение файла конфигурации устройств,
+    и обработку каждого устройства из списка.
+
+    Returns:
+        None
+    """
+    args = parse_cmd_args()
+    devices = read_devices(args.config_file)
     for device in devices:
-        print(f"{'#'*20} Подключаюсь к {device['host']} {'#'*20}")
-        # Вызыввем функцию и передаем туда аргументы из файла, а так же аргументы args.
-        fortios_backup_config(
-            device["host"],
-            device["token"],
-            backup_dir=args.backup_dir,
-            enable_warning=args.enable_warning,
-            enable_ssl=args.enable_ssl,
-        )
+        process_device(device, args.backup_dir, args.enable_warning, args.enable_ssl)
 
 
 if __name__ == "__main__":
